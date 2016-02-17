fd8b89b28ec37e80562802ca7af49e4581ddc9ab
Zach Briggs <briggszj@gmail.com>
2016-02-16 22:41:38 -0600

    Add a jank-ass template

--- app/assets/javascripts/vue/components/filter-list.js
+++ app/assets/javascripts/vue/components/filter-list.js
@@ -2,6 +2,13 @@
   'use strict'
 
   var FilterList = Vue.extend({
+    template: '<p>{{ message }}</p>',
+
+    data: function () {
+      return {
+        message: 'You can do this, but it sucks.'
+      }
+    },
   })
 
   Vue.component('vue-filter-list', FilterList)
