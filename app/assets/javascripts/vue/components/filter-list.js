d5b83b7243ec55c650f15fd5f206e8e46ef2bf83
Zach Briggs <briggszj@gmail.com>
2016-02-16 22:44:23 -0600

    Revert "Add a jank-ass template"
    This reverts commit fd8b89b28ec37e80562802ca7af49e4581ddc9ab.

--- app/assets/javascripts/vue/components/filter-list.js
+++ app/assets/javascripts/vue/components/filter-list.js
@@ -2,13 +2,6 @@
   'use strict'
 
   var FilterList = Vue.extend({
-    template: '<p>{{ message }}</p>',
-
-    data: function () {
-      return {
-        message: 'You can do this, but it sucks.'
-      }
-    },
   })
 
   Vue.component('vue-filter-list', FilterList)
